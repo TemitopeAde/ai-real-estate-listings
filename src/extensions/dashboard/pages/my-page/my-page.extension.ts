c6c8c26f93c829a5ec0a2e8f8b43565926a6feb3
Adesiyan Tope <adesiyantope2014@gmail.com>
2026-08-30 03:58:43 +0100

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '48362ca1-1235-4b30-8f71-21aeac8f512e',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+  fullPage: false,
+});
